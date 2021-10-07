97e31e61a6d49e7e7b3cb8cfb5d7cc0923d241db
behshad <behi.b18@gmail.com>
2021-10-07 12:19:42 -0700

    add templates

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse('<h1>hi</h1>')
+    return render(request, 'pages/index.html')
