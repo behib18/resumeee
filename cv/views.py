4d7439deb80a11d7f22e53906b9e593c56427d2d
behshad <behi.b18@gmail.com>
2021-10-08 12:18:55 -0700

    add cv click

--- cv/views.py
+++ cv/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'cv/index.html')
+    return render(request, 'cv/resume.html')
--- resume/urls.py
+++ resume/urls.py
@@ -21,5 +21,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    path('resume', include('cv.urls')),
+    path('resume/', include('cv.urls')),
 ]
