113365c510ba7335bfda4f4ef1421435b1aed645
behshad <behi.b18@gmail.com>
2021-10-14 15:08:31 -0700

    add

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import datetime
+
+from person_information.models import PersonInfo
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+
+    person_info = PersonInfo.objects.get()
+    context = {
+        'person_info': person_info
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    person_info = PersonInfo.objects.get()
+    today = datetime.date.today().year
+    print(type(today))
+    print(type(person_info.birthday.year))
+    age = today - person_info.birthday.year
+    print(age)
+
+    context = {
+        'person_info': person_info,
+        'age': age,
+    }
+    return render(request, 'pages/about.html', context)
