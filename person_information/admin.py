085b5defa2f5787bcfb5ab6e163ec4e10e2e0801
behshad <behi.b18@gmail.com>
2021-10-14 08:29:31 -0700

    add list_display and list_displays_links in admin.py

--- person_information/admin.py
+++ person_information/admin.py
@@ -4,7 +4,10 @@ from .models import PersonInfo
 
 
 class Person_infornationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'birthday', 'website', 'phone', 'degree')
+    list_display_links = ('user', 'birthday', 'website', 'phone', 'degree')
     # make sure that admin can not enter more than 1 record!
+
     def has_add_permission(self, request):
         if self.model.objects.count() >= 1:
             return False
