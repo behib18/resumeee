24484eee5dd5c78eb7a8a1c7ce758cb5ad73b463
behshad <behi.b18@gmail.com>
2021-10-14 05:18:18 -0700

    setting for limit records to 1 in admin.py in person_info

--- person_information/admin.py
+++ person_information/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import PersonInfo
 
-admin.site.register(PersonInfo)
+
+class Person_infornationAdmin(admin.ModelAdmin):
+    # make sure that admin can not enter more than 1 record!
+    def has_add_permission(self, request):
+        if self.model.objects.count() >= 1:
+            return False
+        return super().has_add_permission(request)
+
+
+admin.site.register(PersonInfo, Person_infornationAdmin)
