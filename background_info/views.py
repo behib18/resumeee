8b980d4ebb1c07af3db82d21c560d1e1f2fb4750
behshad <behi.b18@gmail.com>
2021-10-15 14:26:35 -0700

    add sumary ap

--- background_info/views.py
+++ background_info/views.py
@@ -1,11 +1,14 @@
 from background_info.models import Background
-import background_info
 from django.shortcuts import render
+from person_information.models import PersonInfo
 
 
 def index(request):
     background_info = Background.objects.get()
+    # person_info record
+    person_info = PersonInfo.objects.get()
     context = {
         'background_info': background_info,
+        'person_info': person_info,
     }
     return render(request, 'background_info/index.html', context)
