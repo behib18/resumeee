83e47809427553330d241b1385fdb59ce3aea540
behshad <behi.b18@gmail.com>
2021-10-15 13:37:48 -0700

    chamge

--- tech_skills/admin.py
+++ tech_skills/admin.py
@@ -1,3 +1,11 @@
+from tech_skills.models import TechSkills
 from django.contrib import admin
+from .models import TechSkills
 
-# Register your models here.
+
+class Tech_SkillsAdmin(admin.ModelAdmin):
+    list_display = ('skill', 'quantity')
+    list_display_links = ('skill', 'quantity')
+
+
+admin.site.register(TechSkills, Tech_SkillsAdmin)
--- tech_skills/models.py
+++ tech_skills/models.py
@@ -3,7 +3,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class TechSkills(models.Model):
-    descriptions = models.TextField(max_length=200, blank=True)
     skill = models.CharField(max_length=50)
     quantity = models.IntegerField(
         validators=[MaxValueValidator(100), MinValueValidator(1)])
