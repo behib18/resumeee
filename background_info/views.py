701457ef84ed940a9d68c0eef083315afa1e8f01
behshad <behi.b18@gmail.com>
2021-10-16 03:44:19 -0700

    add education record in html

--- background_info/views.py
+++ background_info/views.py
@@ -2,6 +2,7 @@ from summary.models import Summary
 from background_info.models import Background
 from django.shortcuts import render
 from person_information.models import PersonInfo
+from education.models import Education
 
 
 def index(request):
@@ -11,10 +12,13 @@ def index(request):
     person_info = PersonInfo.objects.get()
     # summary record
     summary = Summary.objects.get()
+    # educations record
+    educations = Education.objects.all()
 
     context = {
         'background_info': background_info,
         'person_info': person_info,
         'summary': summary,
+        'educations': educations,
     }
     return render(request, 'background_info/index.html', context)
