bdc17316f9c97e135a69ec3a89a450f171364830
behshad <behi.b18@gmail.com>
2021-10-16 03:05:20 -0700

    add education models

--- education/models.py
+++ education/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Education(models.Model):
+    title = models.CharField(max_length=150)
+    institute_name = models.CharField(max_length=150)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField(max_length=500)
+
