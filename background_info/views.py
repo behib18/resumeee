863a14a2446ee9344b4989aa263805584e6b81f1
behshad <behi.b18@gmail.com>
2021-10-16 02:58:14 -0700

    add summry record in html

--- background_info/views.py
+++ background_info/views.py
@@ -1,14 +1,20 @@
+from summary.models import Summary
 from background_info.models import Background
 from django.shortcuts import render
 from person_information.models import PersonInfo
 
 
 def index(request):
+    # background_info record
     background_info = Background.objects.get()
     # person_info record
     person_info = PersonInfo.objects.get()
+    # summary record
+    summary = Summary.objects.get()
+
     context = {
         'background_info': background_info,
         'person_info': person_info,
+        'summary': summary,
     }
     return render(request, 'background_info/index.html', context)
--- summary/admin.py
+++ summary/admin.py
@@ -1,3 +1,5 @@
+from summary.models import Summary
 from django.contrib import admin
+from .models import Summary
 
-# Register your models here.
+admin.site.register(Summary)
